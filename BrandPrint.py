bbed69fe2656de9bbe0054a80549caf5dadae46a
Gshufelts <145051075+Gshufelts@users.noreply.github.com>
2024-10-18 14:03:12 -0400

    Update BrandPrint.py

--- BrandPrint.py
+++ BrandPrint.py
@@ -6,4 +6,4 @@ def my_brand(a_name):
     print("=*=*=*=Grant Shufelt=*=*=*=")
     print("=*=*=*=Course 2024S-SSW567-WS=*=*=*=")
     print(f"=*=*=*={a_name}=*=*=*=")
-    print(f"=*=*=*={now.strftime("%Y-%m-%d %H:%M:%S")}=*=*=*=")
+    print(f"=*=*=*={now.strftime('%Y-%m-%d %H:%M:%S')}=*=*=*=")
